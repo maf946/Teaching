15d8f50c04a3f1ed16772dd87bc601ea8244a771
Marc Friedenberg <mfriedenberg@gmail.com>
2020-02-10 09:28:07 -0500

    Updates for python3

--- IST 220/Socket Scripts/TCPClient.py
+++ IST 220/Socket Scripts/TCPClient.py
@@ -3,13 +3,13 @@ message = ""
 
 while 1:
 	if message == "" or message == "123":	
-		serverIP = raw_input("Input server IP: ")
-		serverPort = int(raw_input("Input server port: "))
+		serverIP = input("Input server IP: ")
+		serverPort = int(input("Input server port: "))
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	clientSocket.connect((serverIP, serverPort))
-	print "I'm configured to send TCP packets to " + serverIP + " on port " + str(serverPort)		
-	message = raw_input("Input lowercase sentence (or Ctrl-Z to quit, or 123 to configure): ")	
-	clientSocket.send(message)
+	print("I'm configured to send TCP packets to " + serverIP + " on port " + str(serverPort))		
+	message = input("Input lowercase sentence (or Ctrl-Z to quit, or 123 to configure): ")	
+	clientSocket.send(message.encode())
 	modifiedMessage = clientSocket.recv(1024)
-	print modifiedMessage
+	print(modifiedMessage.decode("utf-8"))
 	clientSocket.close()
