8937d571ab952ced0942de871fd328bd001200e4
Marc Friedenberg <maf946@psu.edu>
2020-02-10 09:02:21 -0500

    Updates for python3

--- IST 220/Socket Scripts/UDPClient.py
+++ IST 220/Socket Scripts/UDPClient.py
@@ -3,12 +3,12 @@ message = ""
 
 while 1:	
 	if message == "" or message == "123":	
-		serverName = raw_input("Input serverName: ")
-		serverPort = int(raw_input("Input serverPort: "))
+		serverName = input("Input serverName: ")
+		serverPort = int(input("Input serverPort: "))
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		
-	print "I'm configured to send UDP packets to " + serverName + " on port " + str(serverPort)		
-	message = raw_input("Input lowercase sentence (or Ctrl-Z to quit, or 123 to configure): ")	
+	print("I'm configured to send UDP packets to " + serverName + " on port " + str(serverPort))
+	message = input("Input lowercase sentence (or Ctrl-Z to quit, or 123 to configure): ")
 	clientSocket.sendto(message, (serverName, serverPort))
 	modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-	print modifiedMessage
+	print (modifiedMessage)
 	clientSocket.close()
