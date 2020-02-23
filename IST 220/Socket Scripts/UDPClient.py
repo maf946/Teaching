38cdfb436df9c6e14cb921c14de331a662bd2b48
Marc Friedenberg <maf946@psu.edu>
2020-02-23 17:01:20 -0500

    Update UDPClient.py
    Clarify with serverIP and serverPort

--- IST 220/Socket Scripts/UDPClient.py
+++ IST 220/Socket Scripts/UDPClient.py
@@ -5,8 +5,8 @@ parser = argparse.ArgumentParser(description='Run a simple UDP client.')
 parser.add_argument("--ipaddress", "-ip", help='IP address for UDP server')
 parser.add_argument("--port", "-p", type=int, help='Port number on which server is running')
 args = parser.parse_args()
-ip = args.ipaddress
-port = args.port
+serverIP = args.ipaddress
+serverPort = args.port
 
 message = ""
 print("I'm configured to send UDP packets to " + str(ip) + " on port " + str(port))
@@ -14,7 +14,7 @@ print("I'm configured to send UDP packets to " + str(ip) + " on port " + str(por
 while 1:	
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		
 	message = input("Input lowercase text: ")
-	clientSocket.sendto(message.encode(), (ip, port))
+	clientSocket.sendto(message.encode(), (serverIP, serverPort))
 	modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 	print (modifiedMessage.decode())
 	clientSocket.close()
