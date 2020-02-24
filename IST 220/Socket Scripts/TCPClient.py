923ec5b2b9ec373716fdc7925a635b9d675cc39a
Marc Friedenberg <maf946@psu.edu>
2020-02-23 19:16:11 -0500

    Updates to mirror UDP client and server

--- IST 220/Socket Scripts/TCPClient.py
+++ IST 220/Socket Scripts/TCPClient.py
@@ -1,15 +1,21 @@
 import socket
-message = ""
+import argparse
+
+parser = argparse.ArgumentParser(description='Run a simple TCP client.')
+parser.add_argument("--ipaddress", "-ip", help='IP address for TCP server')
+parser.add_argument("--port", "-p", type=int, help='Port number on which server is running')
+args = parser.parse_args()
+serverIP = args.ipaddress
+serverPort = args.port
+
+print("I'm configured to send TCP packets to " + serverIP + " on port " + str(serverPort))
+print ("Press Ctrl+Z to quit.")
 
 while 1:
-	if message == "" or message == "123":	
-		serverIP = input("Input server IP: ")
-		serverPort = int(input("Input server port: "))
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	clientSocket.connect((serverIP, serverPort))
-	print("I'm configured to send TCP packets to " + serverIP + " on port " + str(serverPort))		
-	message = input("Input lowercase sentence (or Ctrl-Z to quit, or 123 to configure): ")	
+	message = input("Input lowercase text: ")
 	clientSocket.send(message.encode())
 	modifiedMessage = clientSocket.recv(1024)
-	print(modifiedMessage.decode("utf-8"))
+	print("Returned from server: " + modifiedMessage.decode())
 	clientSocket.close()
--- IST 220/Socket Scripts/TCPServer.py
+++ IST 220/Socket Scripts/TCPServer.py
@@ -1,31 +1,28 @@
 import socket
-import signal
-
-def handler(signum, frame):
-	print(" Quitting.")
-	serverSocket.close()
-	quit()
-
-signal.signal(signal.SIGTSTP, handler)
-
-serverPort = 12004
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
 
 def get_ip_address():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.connect(("8.8.8.8", 80))
 	return s.getsockname()[0]
+	
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('', 0))
 
-print("IP:        " + get_ip_address())
-print("Hostname:  " + socket.gethostname())
-print("Port:      " + str(serverPort))
-print("Press Ctrl+Z to quit. Listening...")
-while 1:
+serverIP = get_ip_address()
+serverPort = serverSocket.getsockname()[1]
+
+serverSocket.listen(1)
+
+print ("serverIP: " + serverIP)
+print ("serverPort:     " + str(serverPort))
+print ("Press Ctrl+Z to quit. Listening...")
+
+while True:
 	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024)
-	print("Data received")
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence)
+	clientIP = str(addr[0])
+	clientPort = str(addr[1])
+	message = connectionSocket.recv(1024).decode()
+	print ("Received from " + clientIP + "#" + clientPort + ": " + message)
+	modifiedMessage = message.upper()
+	connectionSocket.send(modifiedMessage.encode())
 	connectionSocket.close()
