46f49bf22abd760f79435d4cabf23b80da8b4fec
Marc Friedenberg <mfriedenberg@gmail.com>
2020-02-26 08:52:49 -0500

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,14 @@
+# addition.py
+  
+import random 
+
+studentName = input("Hi! What is your name? ")
+print ("Hi, " + studentName + "! Let's do some math today. Let's start with addition.")
+print("You can take a break by typing \"bye\" instead of a number")
+
+var1 = random.randint(0, 50)
+var2 = random.randint(0, 50)
+
+print(var1)
+print(var2)
+
