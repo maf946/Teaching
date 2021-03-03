8963eb1073e70421179cdc8665b55f0abe4aed0e
Marc Friedenberg <maf946@psu.edu>
2021-03-03 18:17:37 -0500

    Update 220install.sh

--- 220install.sh
+++ 220install.sh
@@ -3,3 +3,6 @@ sudo apt upgrade -y
 sudo apt install open-vm-tools-desktop -y
 sudo apt install traceroute -y
 sudo apt install wireshark -y
+sudo dpkg-reconfigure wireshark-common
+sudo usermod -a -G wireshark $USER
+newgrp wireshark
